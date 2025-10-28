1aec732fc9d8027b1b8488638d07f3d68204da3f
IT24103947 <IT24103947@my.sliit.lk>
2025-10-28 15:16:00 +0530

    Lab 09

---
+++ Lab Sheet 09/IT24103947_Lab_09.R
@@ -0,0 +1,20 @@
+getwd()
+
+setwd("C:\\Users\\Shehan\\Desktop\\IT24103947")
+getwd()
+
+##Exercise
+mean_time <- 45
+sd_time <- 2
+sample_size <- 25
+
+
+## i
+set.seed(123)
+sample_data <- rnorm(sample_size, mean = mean_time, sd = sd_time)
+print(sample_data)
+
+
+## ii. One-tailed t-test: H0: mean = 46, H1: mean < 46
+t_test <- t.test(sample_data, mu = 46, alternative = "less")
+print(t_test)
